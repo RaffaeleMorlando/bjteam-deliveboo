b80d4fd7af910020db3f6ce0707b2f2740a4d547
enrico <rombaldonienrico@gmail.com>
2021-03-14 16:51:56 +0100

    refactoring-show
    rifatta la show con una table manca il modifica e la gestione dei prezzi

---
+++ resources/views/admin/restaurants/products/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+  <div class="mt-5 container">
+    <table class="table table-dark table-striped table-bordered">
+      <tbody>
+        <tr>
+          <th scope="row">Immagine</th>
+          <td><img height="180px" src="{{$product->image ? $product->image : "https://www.food4fuel.com/wp-content/uploads/woocommerce-placeholder-600x600.png"}}" alt="{{$product->name}}"></td>
+        </tr>
+        <tr>
+          <th scope="row">Nome piatto</th>
+          <td>{{$product->name}}</td>
+        </tr>
+        <tr>
+          <th scope="row">Ingredienti</th>
+          <td>{{$product->description}}</td>
+        </tr>
+        <tr>
+          <th scope="row">Prezzo</th>
+          <td>{{$product->price}} €</td>
+        </tr>
+        <tr>
+          <th>Vegetariano</th>
+          <td>{{$product->is_vegetarian ? "SI" : "NO"}}</td>
+        </tr>
+        <tr>
+          <th>Senza Glutine</th>
+          <td>{{$product->is_glutenfree ? "SI" : "NO"}}</td>
+        </tr>
+      
+      </tbody>
+    </div> 
+@endsection
