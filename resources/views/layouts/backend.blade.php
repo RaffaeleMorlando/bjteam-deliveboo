4a7975a05e4aa85eaad5b01318c4de323bcbabe8
MarcoB <mrc.bsllt@gmail.com>
2021-03-15 15:50:50 +0100

    Rotte Aside

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -14,20 +14,51 @@
 <body>
   <div id="background">
     <div id="backend" class="container-fluid p-0">
+
         {{-- parte sinistra del layout --}}
-        <main>
+        <main :class="activeAside ? 'main_slide_right' : 'main_slide_left'" id="dashboard-main">
           <h2>titolo della main</h2>
         </main>
 
         {{-- parte destra del layout --}}
-        <aside :class="activeAside ? 'aside_slide_left' : 'aside_slide_right' ">
-          {{-- <header>
-            <a href="#"><i class="far fa-user-circle"></i></a>
-            <span>name utente</span>
-          </header> --}}
+        <aside :class="activeAside ? 'aside_slide_left' : 'aside_slide_right'">
+          <div class="content" :class="activeAside ? 'fade_in' : 'fade_out'">
+
+            <header>
+              <a href="#"><i class="far fa-user-circle"></i></a>
+              <span>name utente</span>
+            </header>
+
+            <main>
+              <h3></h3>
+              <ul class="list-unstyled">
+                <li class="my-5">
+                  <a href="#">
+                    <i class="fas fa-home"></i>
+                    <small>Home</small>
+                  </a>
+                </li>
+                <li class="my-5">
+                  <a href="#">
+                    <i class="fas fa-pizza-slice"></i>
+                    <small>Prodotti</small>
+                  </a>
+                </li>
+                <li class="my-5">
+                  <a href="#">
+                    <i class="far fa-chart-bar"></i>
+                    <small>Ordini</small>
+                  </a>
+                </li>
+
+              </ul>
+            </main>
+          </div>
         </aside>
+
        {{-- burgericon --}}
       <a id="burgerIcon" @click="toggleShow" :class="activeAside ? 'active' : '' "><i></i></a>
+
     </div>
   </div>
 
