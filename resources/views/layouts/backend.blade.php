ce508daca91a75e246da9a281b60e9487bd1c668
MarcoB <mrc.bsllt@gmail.com>
2021-03-15 11:45:47 +0100

    Layout backend
    Inzializzazione layout backend-work in progress

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -9,14 +9,37 @@
   {{-- my style --}}
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <script src="{{ asset('js/app.js') }}" defer></script>
-  
+
 </head>
 <body>
-  <div id="backend" >
-    
+  <div id="background">
+    <div id="backend" class="container-fluid">
+      <div class="row">
 
+        {{-- parte sinistra del layout --}}
+        <main class="col-xs-12 col-lg-10">
+        </main>
+
+        {{-- parte destra del layout --}}
+        <aside class="col-lg-2">
+          <header>
+            <div class="left">
+              <a href="#"><i class="far fa-user-circle"></i></a>
+              <p>Nome utente</p>
+            </div>
+
+            <div class="right">
+              <i class="fas fa-bars"></i>
+            </div>
+
+          </header>
+        </aside>
+
+      </div>
+    </div>
   </div>
 
-  
+
+  <script src="{{ asset("js/partials/layouts/backend.js") }}" charset="utf-8"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {  //da cancellare
-    return view('welcome');
+    return view('layouts.backend');
 });
 
 Auth::routes();
