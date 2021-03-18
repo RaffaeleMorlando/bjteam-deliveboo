47ded96f5f36b11f8e2101c6fcacac91bfa2198e
enrico <rombaldonienrico@gmail.com>
2021-03-18 19:33:13 +0100

    fix height select2

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -50,7 +50,7 @@
           <input type="text" name="phone" id="phone" value="{{ old('phone') }}" placeholder="Inserisci il numero del tuo locale">
         </div>
         <div class="create_product_input">
-          <select class="" id="categories" name="categories[]" multiple>
+          <select style=" width:100%" class="select_form_category" id="categories" name="categories[]" multiple>
             @foreach ($categories as $category)
               <option value="{{ $category->id }}">{{ $category->name }}</option>
             @endforeach
@@ -62,42 +62,4 @@
        
     </div>
   </section>
-
-{{--   <section id="restaurant_form">
-          
-    <div class="container">
-      @if ($errors->any())
-        <div class="alert alert-danger">
-          <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
-      @endif
-
-      <form class="" action="{{ route('admin.restaurants.store') }}" method="post">
-      @csrf
-      @method('POST')
-
-        <div class="box animate__animated animate__slideInRight" v-for="(question, index) in questions" v-if="question.active">
-          <div class="input_container">
-            <i :class="question.icon"></i>
-            <input type="text" :name="question.name" value="" :placeholder="question.placeholder" v-model="question.userInput" required>
-            <button type="button" @click="activeNextQuestion(index)"><i class="fab fa-angellist"></i></button>
-            <i class="fas fa-check mx-3" :style="question.checked ? 'opacity: 1' : 'opacity: 0'"></i>
-          </div>
-        </div>
-
-        <select class="" id="categories" name="categories[]" multiple>
-          @foreach ($categories as $category)
-            <option value="{{ $category->id }}">{{ $category->name }}</option>
-          @endforeach
-        </select>
-
-        <button type="submit" name="button">Registrati</button>
-      </form>
-    </div>
-  </section> --}}
- {{-- v-if="question != questions[questions.length - 1]"  --}}
 @endsection
