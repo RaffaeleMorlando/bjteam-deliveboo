d0fc2ea1bd653b6c812aa00d4bdc30e86f160bb8
Simone Romiti <sr1992ei@gmail.com>
2021-03-19 13:50:38 +0100

    Local storage

--- public/js/partials/guest/menu-restaurant.js
+++ public/js/partials/guest/menu-restaurant.js
@@ -49354,9 +49354,12 @@ var menuRestaurant = new vue__WEBPACK_IMPORTED_MODULE_1__.default({
   data: {
     heroStatus: false,
     currentUrl: window.location.href,
-    restaurant: null,
+    restaurant: {},
     menu: [],
-    categories: []
+    categories: [],
+    products: [],
+    //Array per local storage
+    actualCart: []
   },
   mounted: function mounted() {
     var _this = this;
@@ -49380,7 +49383,47 @@ var menuRestaurant = new vue__WEBPACK_IMPORTED_MODULE_1__.default({
       self.categories = self.restaurant.categories;
     });
   },
-  methods: {}
+  methods: {
+    addToCart: function addToCart(index) {
+      var self = this;
+
+      if (JSON.parse(window.localStorage.getItem('cart')) == null) {
+        self.actualCart = [];
+      } else {
+        self.actualCart.push(JSON.parse(window.localStorage.getItem('cart')));
+      } //Verifica prodotto già presente
+
+
+      if (!self.products.includes(self.menu[index])) {
+        self.products.push(self.menu[index]);
+        self.menu[index].counter = 1;
+      } //Popoliamo local storage in cart
+
+
+      window.localStorage.setItem('cart', JSON.stringify(self.products[index])); //Salvataggio in una variabile
+
+      self.actualCart.push(JSON.parse(window.localStorage.getItem('cart')));
+      console.log(self.actualCart, 'ACTUALCART');
+      console.log(self.products, 'CARRELLO');
+      window.localStorage.setItem('cart', JSON.stringify(self.products));
+    },
+    clearCart: function clearCart() {
+      window.localStorage.clear();
+    },
+    incrementCounter: function incrementCounter(index) {
+      console.log(this.products[index].counter);
+      this.products[index].counter++;
+      this.$forceUpdate();
+    },
+    decrementCounter: function decrementCounter(index) {
+      if (this.products[index].counter > 1) {
+        this.products[index].counter--;
+        this.$forceUpdate();
+      } else {
+        this.products.splice(index, 1);
+      }
+    }
+  }
 });
 })();
 
--- resources/js/partials/guest/menu-restaurant.js
+++ resources/js/partials/guest/menu-restaurant.js
@@ -9,9 +9,11 @@ const menuRestaurant = new Vue({
   data: {
     heroStatus: false,
     currentUrl: window.location.href,
-    restaurant: null,
+    restaurant: {},
     menu: [],
-    categories: []
+    categories: [],
+    products: [], //Array per local storage
+    actualCart: []
   },
 
   mounted() {
@@ -45,7 +47,53 @@ const menuRestaurant = new Vue({
 
   },
   methods: {
+    addToCart: function(index){
+      const self = this;
+      
+      if(JSON.parse(window.localStorage.getItem('cart')) == null){
+        self.actualCart = [];
+      } else{
+        self.actualCart.push(JSON.parse(window.localStorage.getItem('cart')));
+      }
+     
+      //Verifica prodotto già presente
+      if(!self.products.includes(self.menu[index])){
+        self.products.push(self.menu[index]);
+        self.menu[index].counter = 1;
+      } 
 
+      //Popoliamo local storage in cart
+      window.localStorage.setItem('cart', JSON.stringify(self.products[index]));
+
+      //Salvataggio in una variabile
+      self.actualCart.push(JSON.parse(window.localStorage.getItem('cart'))); 
+      console.log(self.actualCart, 'ACTUALCART');
+
+      console.log(self.products, 'CARRELLO'); 
+      
+      window.localStorage.setItem('cart', JSON.stringify(self.products));
+    },
+
+    clearCart: function(){
+      window.localStorage.clear();
+    },
+
+    incrementCounter: function(index){
+      console.log(this.products[index].counter);
+      this.products[index].counter++;
+      this.$forceUpdate();
+    },
+
+    decrementCounter: function(index){
+      if(this.products[index].counter > 1){
+        this.products[index].counter--;
+        this.$forceUpdate();
+      } else{
+        this.products.splice(index, 1);
+      }
+
+     
+    }
   }
 
 });
