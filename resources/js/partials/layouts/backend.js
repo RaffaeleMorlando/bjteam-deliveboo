8359208d86ac82e9de65492a953018cafea6de7d
Simone Romiti <sr1992ei@gmail.com>
2021-03-23 20:11:06 +0100

    Chart completed with filtering by year

--- resources/js/partials/layouts/backend.js
+++ resources/js/partials/layouts/backend.js
@@ -8,6 +8,7 @@ const backend = new Vue({
     activeSettings: false,
     editForm: false,
     url: null,
+    year: "2021"
   },
   methods: {
     toggleShow(){
@@ -25,49 +26,115 @@ const backend = new Vue({
       const file = e.target.files[0];
       this.url = URL.createObjectURL(file);
     },
-  },
-
-  mounted() {
-
-    Vue.prototype.$userId = document.querySelector("meta[name='user-id']").getAttribute('content');
+    filterByYear(){
+      Vue.prototype.$userId = document.querySelector("meta[name='user-id']").getAttribute('content');
     let restaurantSlug = JSON.parse(Vue.prototype.$userId).restaurant.slug;
+
+    let arrayTotal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    console.log(this.year);
 
     axios
       .get(`/api/restaurant/${restaurantSlug}/orders`)
       .then(
         (response) => {
 
-          console.log(response.data);
-          // const self = this;
-          // let myId = [];
-          // response.data.forEach(
-          //   (element) => {
-          //     myId.push(element.id);
-          //   }
-          // );
-          //
-          // var ctx = document.getElementById('myChart').getContext('2d');
-          // var chart = new Chart(ctx, {
-          // // The type of chart we want to create
-          // type: 'line',
-          //
-          // // The data for our dataset
-          // data: {
-          //     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          //     datasets: [{
-          //     label: 'My First dataset',
-          //     backgroundColor: 'rgb(255, 99, 132)',
-          //     borderColor: 'rgb(255, 99, 132)',
-          //     data: myId
-          //   }]
-          // },
-          //
-          //   // Configuration options go here
-          //   options: {}
-          // });
+          // console.log(response.data);
+          const self = this;
+          let myId = [];
+          response.data.forEach(
+            (element) => {
+              if(element.created_at.substr(0, 4) == self.year){
+                for(var i = 1; i <= 12; i++){
+                  if(element.created_at.substr(5, 2) == '0' + i){
+                    arrayTotal[i - 1] = arrayTotal[i - 1] + element.total_price;
+                  } 
+                }
+              }
+              
+              
+            }
+          );
+          console.log(arrayTotal);
+          
+          var ctx = document.getElementById('myChart').getContext('2d');
+          var chart = new Chart(ctx, {
+          // The type of chart we want to create
+          type: 'line',
+          
+          // The data for our dataset
+          data: {
+              labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+              datasets: [{
+              label: '€',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: arrayTotal
+            }]
+          },
+          
+            // Configuration options go here
+            options: {}
+          });
 
         }
       );
+    }
+  },
+
+  mounted() {
+
+    this.filterByYear();
+    // Vue.prototype.$userId = document.querySelector("meta[name='user-id']").getAttribute('content');
+    // let restaurantSlug = JSON.parse(Vue.prototype.$userId).restaurant.slug;
+
+    // let arrayTotal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    // axios
+    //   .get(`/api/restaurant/${restaurantSlug}/orders`)
+    //   .then(
+    //     (response) => {
+
+    //       // console.log(response.data);
+    //       const self = this;
+    //       let myId = [];
+    //       response.data.forEach(
+    //         (element) => {
+    //           if(element.created_at.substr(0, 4) == self.year){
+    //             for(var i = 1; i <= 12; i++){
+    //               if(element.created_at.substr(5, 2) == '0' + i){
+    //                 arrayTotal[i - 1] = arrayTotal[i - 1] + element.total_price;
+    //               } 
+    //             }
+    //           }
+              
+              
+    //         }
+    //       );
+    //       console.log(arrayTotal);
+          
+    //       var ctx = document.getElementById('myChart').getContext('2d');
+    //       var chart = new Chart(ctx, {
+    //       // The type of chart we want to create
+    //       type: 'line',
+          
+    //       // The data for our dataset
+    //       data: {
+    //           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    //           datasets: [{
+    //           label: '€',
+    //           backgroundColor: 'rgb(255, 99, 132)',
+    //           borderColor: 'rgb(255, 99, 132)',
+    //           data: arrayTotal
+    //         }]
+    //       },
+          
+    //         // Configuration options go here
+    //         options: {}
+    //       });
+
+    //     }
+    //   );
 
   }
 });
--- resources/views/admin/restaurants/orders/chart.blade.php
+++ resources/views/admin/restaurants/orders/chart.blade.php
@@ -2,6 +2,11 @@
 
 @section("main")
   <div id="chart_box" class="container" style="width: 100%; height: 100%">
+    <select name="" id="" v-model="year" @change="filterByYear">
+      <option value="2021">2021</option>
+      <option value="2020">2020</option>
+      <option value="2019">2019</option>
+    </select>
     <canvas id="myChart"></canvas>
   </div>
 @endsection
