d297d51459bab017583b82c6165531dc10d6d336
MarcoB <mrc.bsllt@gmail.com>
2021-03-25 00:23:55 +0100

    Tolto il meta per evitare casini nei merge

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,7 +13,6 @@
     <link href="{{ asset('css/style-guest.css') }}" rel="stylesheet">
 
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
   </head>
   <body>
