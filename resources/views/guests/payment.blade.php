3cde71da6bd713da756a7bd3b5036ad98a890c5e
Fujinjakka <calvieri.marco@gmail.com>
2021-03-26 17:57:43 +0100

    indentazione

--- resources/views/guests/payment.blade.php
+++ resources/views/guests/payment.blade.php
@@ -1,143 +1,77 @@
 @extends('../layouts.guest')
 @section('guest-main')
     <div class="container">
-
         <form id="cardForm_payment" action="{{ route('checkout') }}" method="post" style="margin-top: 200px">
             @csrf
             @method('POST')
-            {{-- <label for="total_price">
-                    <span class="input-label">Amount</span>
-                    <div class="input-wrapper amount-wrapper">
-                        <input id="total_price" name="total_price" type="tel" min="1" placeholder="Amount" value="{{number_format($order['total_price'],3)}}" hidden>
-                        <span>{{$order['total_price']}}</span>
+            <div class="panel">
+                <div class="panel__header">
+                    <div>
+                        <h1>Dati di Pagamento</h1>
+                        <img src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png" class="card-item__chip">
                     </div>
-             </label> 
-            <input type="text" name="guest_name">
-            <input type="text" name="guest_address"> --}}
-        <div class="panel">
-            <div class="panel__header">
-              <div>
-              <h1>Dati di Pagamento</h1>
-              <img src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png" class="card-item__chip">
-              </div>
-              <label for="total_price">
-                  <span class="input-label">Totale ordine</span>
-                  <div class="input-wrapper amount-wrapper">
-                      <input id="total_price" name="total_price" type="tel" min="1" placeholder="Amount" value="{{number_format($order['total_price'],3)}}" hidden>
-                      <span>{{ number_format($order['total_price'], 2)." €" }}</span>
-                  </div>
-              </label>
-            </div>        
-            <div class="panel__content">
-                <div class="textfield--float-label">
-                    <!-- Begin hosted fields section -->
-                    <label class="hosted-field--label" for="card-number"><span class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/></svg></span> Numero Carta
-                    </label>
-                    <div id="card-number" class="hosted-field"></div>
-                    <!-- End hosted fields section -->
-                </div>
-                <div class="textfield--float-label">
-                    <!-- Begin hosted fields section -->
-                    <label class="hosted-field--label" for="expiration-date">
-                    <span class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/></svg>
-                    </span>
-                    Data di validità</label>
-                    <div id="expiration-date" class="hosted-field"></div>
-                    <!-- End hosted fields section -->
-                </div>
-                <div class="textfield--float-label">
-                  <label  for="guest_name"><i class="far fa-address-card"></i>Nome e Cognome</label>
-                   <div  class="hosted-field">
-                      <input class="custom_payment_input" type="text" name="guest_name" placeholder="Mario Rossi" required>
-                  </div>
-                </div>  
-                <div class="textfield--float-label">
-                   <label  for="guest_address"><i class="fas fa-map-pin"></i>Indirizzo</label>
-                   <div  class="hosted-field">
-                     <input class="custom_payment_input" type="text" name="guest_address" placeholder="Via Roma, 32" required> 
-                  </div>
-                </div>
-                @foreach ($order as $key => $value)
-                <input type="text" name="{{ $key }}" value="{{ $value }}" 
-                hidden>
-      
-                @endforeach
-                </div>
-            <input id="nonce" name="payment_method_nonce" hidden>
-            <div class="panel__footer">
-              <button type="submit" onclick=" window.localStorage.clear()" class="pay-button">Pay</button>    
-            </div>
-           
-          </div>
-        </form>
-
-            {{-- <form method="post" id="payment-form" action="{{ route('checkout') }}" style="width: 30%;">
-                @csrf
-                @method("POST")
-
-                <section>
                     <label for="total_price">
-                        <span class="input-label">Amount</span>
+                        <span class="input-label">Totale ordine</span>
                         <div class="input-wrapper amount-wrapper">
                             <input id="total_price" name="total_price" type="tel" min="1" placeholder="Amount" value="{{number_format($order['total_price'],3)}}" hidden>
                             <span>{{ number_format($order['total_price'], 2)." €" }}</span>
                         </div>
                     </label>
-
-                    <div class="bt-drop-in-wrapper">
-                        <div id="bt-dropin"></div>
+                </div>        
+                <div class="panel__content">
+                    <div class="textfield--float-label">
+                        <!-- Begin hosted fields section -->
+                        <label class="hosted-field--label" for="card-number">
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                                    <path d="M0 0h24v24H0z" fill="none"/><path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/>
+                                </svg>
+                            </span> Numero Carta
+                        </label>
+                        <div id="card-number" class="hosted-field"></div>
+                        <!-- End hosted fields section -->
                     </div>
-                </section>
-                <input type="text" name="guest_name">
-                <input type="text" name="guest_address">
-                <div>
-                @foreach ($order as $key => $value)
+                    <div class="textfield--float-label">
+                        <!-- Begin hosted fields section -->
+                        <label class="hosted-field--label" for="expiration-date">
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/></svg>
+                            </span>Data di validità
+                        </label>
+                        <div id="expiration-date" class="hosted-field"></div>
+                        <!-- End hosted fields section -->
+                    </div>
+                    <div class="textfield--float-label">
+                        <label class="custom_payment_label" for="guest_name"><i class="far fa-address-card"></i>Nome e Cognome</label>
+                        <div  class="hosted-field custom_payment_border">
+                            <input class="custom_payment_input" type="text" name="guest_name" placeholder="Mario Rossi" required>
+                        </div>
+                    </div>  
+                    <div class="textfield--float-label">
+                        <label class="custom_payment_label" for="guest_address"><i class="fas fa-map-pin"></i>Indirizzo</label>
+                        <div  class="hosted-field custom_payment_border">
+                            <input class="custom_payment_input" type="text" name="guest_address" placeholder="Via Roma, 32" required> 
+                        </div>
+                    </div>
+
+                    @foreach ($order as $key => $value)
                     <input type="text" name="{{ $key }}" value="{{ $value }}" hidden>
-                @endforeach
+                    @endforeach
 
                 </div>
-                <input id="nonce" name="payment_method_nonce" type="hidden" />
-                <button class="button" type="submit" onclick=" window.localStorage.clear()"><span>Test Transaction</span></button>
-            </form> --}}
-            <script src="https://js.braintreegateway.com/web/3.76.0/js/client.min.js"></script>
+                <input id="nonce" name="payment_method_nonce" hidden>
+                <div class="panel__footer">
+                    <button type="submit" onclick=" window.localStorage.clear()" class="pay-button">Pay</button>    
+                </div>
+            
+            </div>
+        </form>
 
-            <script src="https://js.braintreegateway.com/web/3.76.0/js/hosted-fields.min.js"></script>
-
-        {{-- <script src="https://js.braintreegateway.com/web/dropin/1.13.0/js/dropin.min.js"></script> --}}
-        {{-- <script>
-            var form = document.querySelector('#payment-form');
-            var client_token = "{{ $token }}";
-
-            braintree.dropin.create({
-            authorization: client_token,
-            selector: '#bt-dropin',
-            paypal: {
-            flow: 'vault'
-            }
-            }, function (createErr, instance) {
-            if (createErr) {
-            console.log('Create Error', createErr);
-            return;
-            }
-            form.addEventListener('submit', function (event) {
-            event.preventDefault();
-            instance.requestPaymentMethod(function (err, payload) {
-            if (err) {
-                console.log('Request Payment Method Error', err);
-                return;
-            }
-            // Add the nonce to the form and submit
-            document.querySelector('#nonce').value = payload.nonce;
-            form.submit();
-            });
-            });
-            });
-        </script> --}}
-
+        <script src="https://js.braintreegateway.com/web/3.76.0/js/client.min.js"></script>
+        <script src="https://js.braintreegateway.com/web/3.76.0/js/hosted-fields.min.js"></script>
     </div>
-        <script>
+
+    <script>
         var form = document.querySelector('#cardForm_payment');
         var client_token = "{{ $token }}";
         console.log(braintree);
@@ -247,6 +181,6 @@
             });
         });
         });
-        </script>
+    </script>
 @endsection
 
